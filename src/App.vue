8a321ef50b3ba2bcd28aa08270356aad3c39b4bc
ivan-46 <ivanpirat2017@yandex.ru>
2024-04-20 12:46:07 +0300

    I made a template

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <script setup lang="ts">
 
-
+import { ref } from 'vue'
+import RoutePoint from './components/RoutePoint/RoutePoint.vue'
 import OsrmNavigator from './utility/Osrm'
+import { uuidv4 } from './utility/uuid'
+
 
 const osrm = new OsrmNavigator(window.olMap)
-osrm.on('updatePoints', data=> {
-  console.log("test", data)
+const points = ref(osrm.getPoints())
+
+osrm.on('updatePoints', data => {
+  points.value = data
 })
- 
+
 osrm.addPoint({
-  id: '123',
-  address: 'test',
-  coordinate: [1, 1]
+  id: uuidv4(),
+  address: '',
+  coordinate: []
 })
- 
+
 
 </script>
 
 <template>
-  <div>
-    <h1>2222</h1>
+  <div class="osrm">
+    <div class="osrmTypeRouter">
+      <p>car</p>
+      <p>bike</p>
+      <p>foot</p>
+    </div>
+    <div class="osrmList">
+      <RoutePoint v-for="point in points" :key="point.id" />
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.osrm {
+  padding: 10px;
+
+  &List {
+    padding: 10px 5px;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+
+  &TypeRouter {
+    display: flex;
+    justify-content: start;
+    gap: 5px;
+
+    p {
+      background: rgba(255, 255, 255, 0.2);
+      padding: 5px;
+      border-radius: 5px;
+      cursor: pointer;
+
+      &:hover {
+        background: rgba(255, 255, 255, 0.4);
+      }
+    }
+
+  }
+
+}
+</style>
---
+++ src/components/RoutePoint/RoutePoint.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="RoutePoint">
+        <div class="RoutePointCircle"></div>
+        <div class="RoutePointData">
+            <div class="RoutePointDataAddress">
+                <input type="text" placeholder="55.23232 22.232323">
+            </div>
+
+            <div class="RoutePointDataBtns">
+                <button>
+                    <img src="./icon/clear.png" alt="">
+                </button>
+            </div>
+        </div>
+
+
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+.RoutePoint {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 10px;
+
+    &Data {
+        display: flex;
+        // border: white 2px solid;
+        background: rgba(255, 255, 255, 0.2);
+        border-radius: 5px;
+        padding: 10px;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+
+        &Address {
+            display: flex;
+            align-items: center;
+            gap: 15px;
+
+            input {
+                font-size: 1.1em;
+            }
+
+        }
+
+
+        &Btns {
+            display: flex;
+            gap: 5px;
+
+            button {
+                cursor: pointer;
+
+                img {
+                    height: 15px;
+                }
+            }
+        }
+    }
+
+    &Circle {
+        display: flex;
+        max-width: 15px;
+        max-height: 15px;
+        min-width: 15px;
+        min-height: 15px;
+        width: 15px;
+        height: 15px;
+        background-color: red;
+        border-radius: 100%;
+    }
+
+
+
+}
+</style>
